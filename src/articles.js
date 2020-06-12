bef59d07fac41f47464fb5dfb04dae6110f05c0c
Diane Mpacko <diane.mpacko@gmail.com>
2020-06-12 21:55:52 +0200

    description projet kimia

--- src/articles.js
+++ src/articles.js
@@ -12,7 +12,7 @@ const articlesProjets = [
         id: 2,
         img: "my-wedding-thumbnail.png",
         titre: "My Wedding App",
-        legende: "My Wedding App est le projet de fin de formation que je présenterai lors de la certification. Il s'agit d'un projet de site web pour organisation de mariage. Vous pouvez dès à présent regarder l'avancement du projet.",
+        legende: "My Wedding App est le projet de fin de formation que je présenterai lors de la certification. Il s'agit d'un site web pour organisation de mariage. Vous pouvez dès à présent regarder l'avancement du projet.",
         website: "https://www.heroku.com",
         github: "https://github.com/dkm94/front_mariage_app/tree/Developpement"
         // technos: "REACT JS • NODE JS"
