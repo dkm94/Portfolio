10d8cc13721e3952f18ffb80c8dee497baf2ea22
Diane Mpacko <diane.mpacko@gmail.com>
2020-08-12 13:58:43 +0200

    update css and add new project

--- src/articles.js
+++ src/articles.js
@@ -11,10 +11,20 @@ const articlesProjets = [
     {
         id: 2,
         img: "my-wedding-thumbnail.png",
-        titre: "My Wedding App",
-        legende: "My Wedding App est le projet de fin de formation que je présenterai lors de la certification. Il s'agit d'un site web pour organisation de mariage. Vous pouvez dès à présent regarder l'avancement du projet.",
-        website: "https://www.heroku.com",
-        github: "https://github.com/dkm94/front_mariage_app/tree/Developpement"
+        titre: "My Wedding App (serveur)",
+        // legende: "My Wedding App est le projet de fin de formation que je présenterai lors de la certification. Il s'agit d'un site web pour organisation de mariage. Vous pouvez dès à présent regarder l'avancement du projet.",
+        legende: "C'est le serveur complet de la future application MyWeddingApp. Il s'agit d'une app d'organisation de mariage: les mariés peuvent créer leur compte, et planifier les détails clés de la cérémonie tels que l'ajout d'invités, d'un menu, le placement sur les tables...Les invités peuvent de leur côté compléter leur profil et chosir leurs préférences pour le choix du menu (ce qui permet aux mariés d'avoir une idée précise des quantités à commander), ajouter une photo de profil (pour permettre l'identifiation visuelle pour les autres invités sur les tables). L'application REACT est en contruction.",
+        // website: "https://www.heroku.com",
+        github: "https://github.com/dkm94/keroku_mariage/tree/dev"
+        // technos: "REACT JS • NODE JS"
+    },
+    {
+        id: 3,
+        img: "screenshot-nutrifruits.png",
+        titre: "Nutrifruits",
+        legende: "Application qui regroupe des informations nutritionnelles sur les fruits du quotidien.",
+        website: "https://nutrifruitsdemo.herokuapp.com/",
+        github: "https://github.com/sarahnuran/nutrifruits/"
         // technos: "REACT JS • NODE JS"
     }
 ]
